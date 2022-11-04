4e536a1a689b57bd074ffedabc851eb5262e5f51
csilvestre <clsoft.silvestre@gmail.com>
2022-11-04 14:35:24 -0100

    Initial architecture

---
+++ factory-minimal-edge/factory-minimal-edge-models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace factory_minimal_edge_models
+{
+    public class Class1
+    {
+    }
+}
---
+++ factory-minimal-edge/factory-minimal-edge-plc-connector/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using S7.Net;
+
+namespace factory_minimal_edge_plc_connector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plc plc = new Plc(CpuType.Logo0BA8, "127.0.0.1", 0, 2);
+            //plc.Open();
+
+            Console.WriteLine("Hello World!");
+            //plc.Close();
+        }
+    }
+}
