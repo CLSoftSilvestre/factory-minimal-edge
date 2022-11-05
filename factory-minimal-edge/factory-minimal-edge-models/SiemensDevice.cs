bad981e8a7b487641dd2d2c44501ab94152d2618
csilvestre <clsoft.silvestre@gmail.com>
2022-11-05 09:48:49 -0100

    Added Siemens PLC models

---
+++ factory-minimal-edge/factory-minimal-edge-models/SiemensDevice.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using S7.Net;
+
+namespace factory_minimal_edge_models
+{
+    public class SiemensDevice
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public CpuType Type { get; set; }
+        public string IP_Address { get; set; }
+        public short Rack { get; set; }
+        public short Slot { get; set; }
+
+        public virtual ICollection<SiemensTag> Tags { get; set; }
+
+        [NotMapped]
+        private Plc plc;
+
+        public bool CreateDriver()
+        {
+            plc = new Plc(Type, IP_Address, Rack, Slot);
+
+            if (plc != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Plc GetDriver()
+        {
+            return plc;
+        }
+
+        public bool Connect()
+        {
+            if (plc == null)
+            {
+                CreateDriver();
+            }
+  
+            try
+            {
+                plc.Open();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Disconnect()
+        {
+            plc.Close();
+
+            if (plc.IsConnected == false)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsConnected()
+        {
+            return plc.IsConnected;
+        }
+
+    }
+}
---
+++ factory-minimal-edge/factory-minimal-edge-models/SiemensTag.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using S7.Net;
+
+namespace factory_minimal_edge_models
+{
+    public class SiemensTag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DeviceId { get; set; }
+        public virtual SiemensDevice Device { get; set; }
+        public VarType Type { get; set; }
+        public string Address { get; set; }
+
+        [NotMapped]
+        public object Value {get; set;}
+
+        public async Task<bool> UpdateTagValue()
+        {
+            var tagValue = await Device.GetDriver().ReadAsync(Address);
+            Value = tagValue;
+
+            if(tagValue != null)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+
+}
